93812264ae97a45b32c1d6171fbe493b10b9f5ea
Victor Farias <gfvictor@proton.me>
2025-02-10 03:14:12 +0900

    feat: imported Prisma and Person modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, PersonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

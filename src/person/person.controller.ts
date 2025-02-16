222e13918fe4c8fc2734e6e91f62c6838686125d
Victor Farias <gfvictor@proton.me>
2025-02-17 04:57:06 +0900

    feat: implemented patch and delete endpoints

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,24 +1,26 @@
-import { Body, Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PersonService } from './person.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CreatePersonDto, UpdatePersonDto } from './person.dto';
 
-@Controller('Person')
+@Controller('person')
 export class PersonController {
   constructor(private readonly personService: PersonService) {}
 
   @Post()
-  async create(
-    @Body()
-    data: {
-      name: string;
-      email: string;
-      username: string;
-      password: string;
-      homeUse: boolean;
-      workUse: boolean;
-      isAdmin: boolean;
-    },
-  ) {
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async create(@Body() data: CreatePersonDto) {
     return this.personService.create(data);
   }
 
@@ -32,5 +34,16 @@ export class PersonController {
   @UseGuards(JwtAuthGuard)
   async findOne(@Param('id') id: string) {
     return this.personService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async update(@Param('id') id: string, @Body() data: UpdatePersonDto) {
+    return this.personService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.personService.delete(id);
   }
 }

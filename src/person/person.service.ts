9eeae1e4547b491144b3663fc3ff034979767523
Victor Farias <gfvictor@proton.me>
2025-02-17 04:56:11 +0900

    feat: changed validation to dto and created update and delete services

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,20 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import { CreatePersonDto, UpdatePersonDto } from './person.dto';
 
 @Injectable()
 export class PersonService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: {
-    name: string;
-    email: string;
-    username: string;
-    password: string;
-    homeUse: boolean;
-    workUse: boolean;
-    isAdmin: boolean;
-  }) {
+  private async findUserOrThrow(id: string) {
+    const user = await this.prisma.person.findUnique({ where: { id } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async create(data: CreatePersonDto) {
     const hashedPassword = await bcrypt.hash(data.password, 10);
     return this.prisma.person.create({
       data: {
@@ -23,10 +22,32 @@ export class PersonService {
       },
     });
   }
+
   async findAll() {
     return this.prisma.person.findMany();
   }
+
   async findOne(id: string) {
-    return this.prisma.person.findUnique({ where: { id } });
+    return this.findUserOrThrow(id);
+  }
+
+  async update(id: string, data: UpdatePersonDto) {
+    await this.findUserOrThrow(id);
+
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+
+    return this.prisma.person.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.findUserOrThrow(id);
+    await this.prisma.person.delete({ where: { id } });
+
+    return { message: 'User successfully deleted' };
   }
 }

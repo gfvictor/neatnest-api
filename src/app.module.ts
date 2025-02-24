326f672696a964b14a384b2a75d2e4f77bdac01e
Victor Farias <gfvictor@proton.me>
2025-02-24 14:40:57 +0900

    feat: created service for role update by admin only

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { PersonModule } from './person/person.module';
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PersonModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })

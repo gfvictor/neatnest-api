6ed0a265d01601d2b20a8d19899d19299fdf27da
Victor Farias <gfvictor@proton.me>
2025-02-24 14:32:44 +0900

    chore: changed Person table name to User, file names and imports

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PersonService } from './person.service';
+import { UserService } from './user.service';
 
 describe('PersonService', () => {
-  let service: PersonService;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PersonService],
+      providers: [UserService],
     }).compile();
 
-    service = module.get<PersonService>(PersonService);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {

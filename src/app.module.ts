d5b7bd0c0345b861b496ce055950af5a35182d05
Victor Farias <gfvictor@proton.me>
2025-03-03 01:08:45 +0900

    feat: imported workplace and section entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { HouseholdModule } from './household/household.module';
 import { RoomModule } from './room/room.module';
+import { WorkplaceModule } from './workplace/workplace.module';
+import { SectionModule } from './section/section.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, HouseholdModule, RoomModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    HouseholdModule,
+    RoomModule,
+    WorkplaceModule,
+    SectionModule,
+  ],
   controllers: [],
   providers: [],
 })

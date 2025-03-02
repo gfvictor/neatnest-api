aedfe90d58062d803a2932d8964a4323e320d46f
Victor Farias <gfvictor@proton.me>
2025-03-02 11:23:14 +0900

    chore: updated imports directories

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthModule } from '../auth/auth.module';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @Module({
   imports: [AuthModule],

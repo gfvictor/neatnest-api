a820a0f3c6df715c307acb84822d2392cea311c9
Victor Farias <gfvictor@proton.me>
2025-03-02 11:48:29 +0900

    chore: imported room and household modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { HouseholdModule } from './household/household.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, HouseholdModule, RoomModule],
   controllers: [],
   providers: [],
 })

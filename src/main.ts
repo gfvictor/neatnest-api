f7916245fbb08faf4579342f7468be76000f9d13
Victor Farias <gfvictor@proton.me>
2025-03-20 10:51:56 +0900

    chore: enabled CORS to Vercel URL

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://neatnest.vercel.app', 'http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server running on port ${process.env.PORT}`);
 }
+
 void bootstrap();

4a7fbc84a1f546f7b50d844962a65b087ea53022
Victor Farias <gfvictor@proton.me>
2025-10-25 12:33:32 +0900

    chore: added mobile testing origin to CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://neatnest.vercel.app', 'http://localhost:4200'],
+    origin: [
+      'https://neatnest.vercel.app',
+      'http://localhost:4200',
+      'http://192.168.1.99:4200',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,

4db76a16971a6e04934ad75e4f1b46e51836f95d
Victor Farias <gfvictor@proton.me>
2026-05-06 16:01:19 +0900

    feat: add PrismaModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { SupabaseAuthStrategy } from './supabase-auth.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule, PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PrismaModule,
+  ],
   providers: [SupabaseAuthStrategy],
   controllers: [],
   exports: [PassportModule, SupabaseAuthStrategy],

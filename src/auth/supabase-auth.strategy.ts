2a3d43e4f85d419814f8bb37bddf944cd2827276
Victor Farias <gfvictor@proton.me>
2026-05-06 16:03:25 +0900

    chore: refator payload acquisition strategy

--- src/auth/supabase-auth.strategy.ts
+++ src/auth/supabase-auth.strategy.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class SupabaseAuthStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,15 +17,19 @@ export class SupabaseAuthStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  validate(payload: {
+  async validate(payload: {
     sub: string;
     email: string;
     app_metadata: { role: 'USER' | 'ADMIN' | 'TESTER' };
   }) {
-    return {
-      id: payload.sub,
-      email: payload.email,
-      role: payload.app_metadata?.role || 'USER',
-    };
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found in local database.');
+    }
+
+    return user;
   }
 }
